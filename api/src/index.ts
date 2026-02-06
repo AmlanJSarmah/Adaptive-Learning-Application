63118202836f5477d938c6e3bb9a53612a3131c6
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2026-02-06 14:22:32 +0530

    Added basic error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import type {
+  ErrorRequestHandler,
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
 import cors from 'cors';
 import authRouter from './routes/authRoutes.ts';
 
@@ -8,6 +14,18 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/user', authRouter);
+
+app.use(
+  (
+    error: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+);
 
 app.listen(8080, () => {
   console.log('Server started!');
